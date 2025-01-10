1810e8a29ba83d2fdf9f2eb5084f00b79158a5ef
Samuel <samuel7123@gmail.com>
2025-01-10 18:42:05 -0300

    anexo de contenido

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,8 +1,8 @@
 function Jumbotron(props) {
 
     return <div className="jumbotron" style={{ backgroundColor: '#f8f9fa', padding: '20px' }}>
-    <h1 className="display-4" style={{ padding: '5px' }}>Hello, world!</h1>
-    <p className="lead" style={{ padding: '5px' }}>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <h1 className="display-4" style={{ padding: '5px' }}>El Aguacate... Realmente el oro verde?</h1>
+    <p className="lead" style={{ padding: '5px' }}>Es una especie arbórea del género Persea perteneciente a la familia Lauraceae, cuyo fruto, el aguacate,​ palta o avocado, es una baya comestible.</p>
     <p className="lead" style={{ padding: '5px' }}>
         <a className="btn btn-primary btn-lg" href="#" role="button" style={{ margin: '10px' }}>Learn more</a>
     </p>
